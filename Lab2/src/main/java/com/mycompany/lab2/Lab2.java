ebcc41e91901e5a81e2096cc215e7915487bb82d
MinhNghiaZ <167590885+MinhNghiaZ@users.noreply.github.com>
2024-05-05 22:48:31 +0700

    Final
    Final

--- Lab2/src/main/java/com/mycompany/lab2/Lab2.java
+++ Lab2/src/main/java/com/mycompany/lab2/Lab2.java
@@ -13,28 +13,28 @@ public class Lab2 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-//        Complex c1 = new Complex();
-//        c1.InputComplex();
-//        Complex c2 = new Complex();
-//        c2.InputComplex();
-//        c1.OutputComplex();
-//        c2.OutputComplex();
-//        c1.PlusComplex(c2).OutputComplex();
-//        c1.MinusComplex(c2).OutputComplex();
-//        c1.MultiComplex(c2).OutputComplex();
-//        c1.DivideComplex(c2).OutputComplex();
-//        Bank bank = new Bank();
-//        bank.Input();
-//        bank.Output();
-//        bank.DepositAccount();
-//        bank.WithDrawAccount();
-//        bank.TransferMoney();
-//        bank.Output();
-          Manager manage = new Manager();
-          manage.InputList();
-          manage.OutputSalary();
-          manage.OutputHighest();
-          manage.sortDecending();
+        Complex c1 = new Complex();
+        c1.InputComplex();
+        Complex c2 = new Complex();
+        c2.InputComplex();
+        c1.OutputComplex();
+        c2.OutputComplex();
+        c1.PlusComplex(c2).OutputComplex();
+        c1.MinusComplex(c2).OutputComplex();
+        c1.MultiComplex(c2).OutputComplex();
+        c1.DivideComplex(c2).OutputComplex();
+        Bank bank = new Bank();
+        bank.Input();
+        bank.Output();
+        bank.DepositAccount();
+        bank.WithDrawAccount();
+        bank.TransferMoney();
+        bank.Output();
+        Manager manage = new Manager();
+        manage.InputList();
+        manage.OutputSalary();
+        manage.OutputHighest();
+        manage.sortDecending();
 
     }
 }
