dd2997517f6f247b59ae980f963880f3b0e3c84e
Minh Nghia <167590885+MinhNghiaZ@users.noreply.github.com>
2024-05-11 10:17:17 +0700

    V1
    Version 1

--- Lab3/src/main/java/com/mycompany/lab3/Lab3.java
+++ Lab3/src/main/java/com/mycompany/lab3/Lab3.java
@@ -11,6 +11,16 @@ package com.mycompany.lab3;
 public class Lab3 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        CD Cd1 = new CD();
+        Cd1.addCD();
+        Cd1.SearchByTitle();
+        Cd1.SearchByCollection();
+        Cd1.SearchByType();
+        Cd1.DeleteID();
+        Cd1.EditByID();
+        Cd1.OutputCD();
+        Cd1.SortAccending();
+        Cd1.OutputCD();
+        
     }
 }
